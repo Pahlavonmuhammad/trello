0b22b878b47bd4b8634972452e3b4fd0bc6ae36c
Muhammad <salayevmuhammad6@gmail.com>
2025-04-29 17:29:51 +0500

    new Entities are created

---
+++ src/main/java/com/example/trello/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.example.trello.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    private User user;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private String comment;
+}
---
+++ src/main/java/com/example/trello/entity/Task.java
@@ -0,0 +1,27 @@
+package com.example.trello.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    private Status status;
+    @ManyToOne
+    private Attachment attachment;
+    @ManyToOne
+    private User user;
+    private String title;
+    @OneToMany
+    List<Comment> comments;
+}
